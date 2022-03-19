8d67f949ae95d2648031e543e492105cabd4c151
vaibhavpawar05 <vaibhavpawar05@gmail.com>
2022-03-19 19:49:25 +0530

    Update fastai-resnet50-label-smoothing.py

--- fastai-resnet50-label-smoothing.py
+++ fastai-resnet50-label-smoothing.py
@@ -36,16 +36,16 @@ jorda1_footbed_stitching = DataBlock(blocks = (ImageBlock, CategoryBlock),
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("The model will be running on", device, "device")
 
-
-# Creating train dataloader.
+# Creating train dataloader
 train_dls = jorda1_footbed_stitching.dataloaders(path, num_workers=4, batch_size=bs, device=device)
 
-# Adding for gpu support.
+# Adding for gpu support
 train_dls = train_dls.cuda()
 
 model_path = Path('/dbfs/tmp/vaibhav/fastai-resnet50-label-smoothing')
 
-learn = cnn_learner(train_dls, resnet50, path=model_path, metrics=[error_rate, accuracy, Precision(), Recall()])
+learn = cnn_learner(train_dls, resnet50, loss_func=LabelSmoothingCrossEntropy(), 
+                    path=model_path, metrics=[error_rate, accuracy, Precision(), Recall()])
 
 learn.model = learn.model.cuda()
 
